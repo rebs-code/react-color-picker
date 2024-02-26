19e1e840b0bfa5ef69238b7f2f6d07b55822684a
rebs-code <jack.rebussi@gmail.com>
2024-02-26 12:30:50 +0100

    feat: first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+//import color picker component
+import ColorPicker from "./components/ColorPicker";
+
+function App() {
+  return (
+    <>
+      <h1>React Color Picker</h1>
+      <ColorPicker />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ColorPicker.jsx
@@ -0,0 +1,15 @@
+//import useState
+import React, { useState } from "react";
+
+function ColorPicker() {
+  //create state for color
+  const [color, setColor] = useState("#FFFFFF");
+
+  return (
+    <>
+      <div>hello</div>
+    </>
+  );
+}
+
+export default ColorPicker;
