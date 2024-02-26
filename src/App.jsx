99c41054086b6dd9c0ad878bc112ae125f2241e9
rebs-code <jack.rebussi@gmail.com>
2024-02-26 13:11:28 +0100

    Add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import "./App.css";
 import Header from "./components/Header/Header";
 //import color picker component
 import ColorPicker from "./components/ColorPicker/ColorPicker";
+//import footer component
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <ColorPicker />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+//import footer css module
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>Made by rebs-code</p>
+    </footer>
+  );
+}
+
+export default Footer;
