87ac7c92c596e016e1ca3ab3e7161b3ef6e45ae0
rebs-code <jack.rebussi@gmail.com>
2024-02-26 12:53:49 +0100

    add: contrast color function for text inside color div

--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -12,13 +12,26 @@ function ColorPicker() {
     setColor(e.target.value);
   };
 
+  //create a text color variable
+  const textColor = getContrastColor(color);
+
+  //create function to get contrast color
+  function getContrastColor(hexcolor) {
+    var r = parseInt(hexcolor.substr(1, 2), 16);
+    var g = parseInt(hexcolor.substr(3, 2), 16);
+    var b = parseInt(hexcolor.substr(5, 2), 16);
+    var yiq = (r * 299 + g * 587 + b * 114) / 1000;
+    return yiq >= 128 ? "black" : "white";
+  }
+
   return (
     <>
       <div className={styles.colorPickerContainer}>
-        <div className="colorDisplay" style={{ backgroundColor: color }}></div>
-        <p>
-          Selected Color: <strong>{color}</strong>
-        </p>
+        <div className={styles.colorDisplay} style={{ backgroundColor: color }}>
+          <p style={{ color: textColor }}>
+            Selected Color: <strong>{color}</strong>
+          </p>
+        </div>
         <label>Select a Color </label>
         <input type="color" value={color} onChange={handleColorChange} />
       </div>
